aabadeb5c5f0da70a35dfd664106e73d465badbf
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-29 11:06:25 +0300

    Fix test to load index from lib

--- src/app/templates/test/unit/index.test.js
+++ src/app/templates/test/unit/index.test.js
@@ -1,5 +1,5 @@
 var assert = require('chai').assert;
-var index = require('../../index');
+var index = require('../../lib/index');
 
 describe('Entry Point', function () {
   it('Should properly export', function () {
